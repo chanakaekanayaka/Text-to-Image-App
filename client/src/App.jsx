fc7f8145e4d6f8a9cefbb9cb4e171b6495a57d2b
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2025-10-26 11:26:17 +0530

    Setup routes ✨

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-29 min-h-screen bg-gradient-to-b from-teal-50 to-orange '>
+
+      <Routes>
+        <Route path='/' element={<Home></Home>}/>
+        <Route path='/result' element={<Result></Result>}/>
+        <Route path='/buy' element={<BuyCredit></BuyCredit>}/>
+      </Routes>
+
+      </div>
   )
 }
 
---
+++ client/src/pages/BuyCredit.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BuyCredit = () => {
+  return (
+    <div>
+
+        <h1>Buy Credit</h1>
+      
+    </div>
+  )
+}
+
+export default BuyCredit
